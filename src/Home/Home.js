0e085552b206344165711439ea04e4b78f7c785d
hellorafiul <hellorafiul@gmail.com>
2021-10-30 03:58:58 +0600

    another test

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1 className="my-3">This is Home.</h1>
+            <h1 className="my-3">This is Home. slider</h1>
         </div>
     );
 };
